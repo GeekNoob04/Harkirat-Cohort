3cc57cf017253d26fe0a64e38b446c05fadf3ce8
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-06-23 01:29:24 +0530

    assignment-1 practice

--- week4/assignment-1/middleware/user.js
+++ week4/assignment-1/middleware/user.js
@@ -1,21 +1,42 @@
+// const { User } = require("../db");
+// function userMiddleware(req, res, next) {
+//     // Implement user auth logic
+//     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+//     const username = req.headers.username;
+//     const password = req.headers.password;
+//     User.findOne({
+//         username: username,
+//         password: password,
+//     }).then(function (value) {
+//         if (value) {
+//             next();
+//         } else {
+//             res.status(404).json({
+//                 msg: "user does not exist",
+//             });
+//         }
+//     });
+// }
+
+// module.exports = userMiddleware;
+
 const { User } = require("../db");
-function userMiddleware(req, res, next) {
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
     const username = req.headers.username;
     const password = req.headers.password;
-    User.findOne({
-        username: username,
-        password: password,
-    }).then(function (value) {
-        if (value) {
-            next();
-        } else {
-            res.status(404).json({
-                msg: "user does not exist",
-            });
-        }
+    const user = await User.findOne({
+        username,
+        password,
     });
+    if (user) {
+        next();
+    } else {
+        res.status(404).json({
+            msg: "user does not exis",
+        });
+    }
 }
 
 module.exports = userMiddleware;
--- week4/assignment-1/routes/user.js
+++ week4/assignment-1/routes/user.js
@@ -1,7 +1,69 @@
+// const { Router } = require("express");
+// const router = Router();
+// const userMiddleware = require("../middleware/user");
+// const { Course, User } = require("../db");
+
+// // User Routes
+// router.post("/signup", (req, res) => {
+//     // Implement user signup logic
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     User.create({
+//         username,
+//         password,
+//     });
+//     res.json({
+//         msg: "user created successfully",
+//     });
+// });
+
+// router.get("/courses", async (req, res) => {
+//     // Implement listing all courses logic
+//     const response = await Course.find({});
+//     res.json({
+//         course: response,
+//     });
+// });
+
+// router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+//     const username = req.headers.username;
+//     const courseId = req.params.courseId;
+//     await User.updateOne(
+//         {
+//             username: username,
+//         },
+//         {
+//             $push: {
+//                 purchasedCourses: courseId,
+//             },
+//         }
+//     );
+//     res.json({
+//         msg: "course updated",
+//     });
+// });
+
+// router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+//     // Implement fetching purchased courses logic
+//     const user = await User.findOne({
+//         username: req.headers.username,
+//     });
+//     const courses = await Course.find({
+//         _id: {
+//             $in: user.purchasedCourses,
+//         },
+//     });
+//     res.json({
+//         msg: courses,
+//     });
+// });
+
+// module.exports = router;
+
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-const { Course, User } = require("../db");
+const { User, Course } = require("../db");
 
 // User Routes
 router.post("/signup", (req, res) => {
@@ -26,11 +88,12 @@ router.get("/courses", async (req, res) => {
 });
 
 router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+    // Implement course purchase logic
     const username = req.headers.username;
-    const courseId = req.params.courseId;
+    const courseId = req.params.password;
     await User.updateOne(
         {
-            username: username,
+            username,
         },
         {
             $push: {
@@ -39,14 +102,15 @@ router.post("/courses/:courseId", userMiddleware, async (req, res) => {
         }
     );
     res.json({
-        msg: "course updated",
+        msg: "course purchased successfully",
     });
 });
 
 router.get("/purchasedCourses", userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    const username = req.headers.username;
     const user = await User.findOne({
-        username: req.headers.username,
+        username,
     });
     const courses = await Course.find({
         _id: {
