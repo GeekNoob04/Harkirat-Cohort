ec989724b200bdc71196fbe81d3e711feeb12c51
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-06-24 01:20:00 +0530

    assignment-2

--- week4/assignment-2/middleware/admin.js
+++ week4/assignment-2/middleware/admin.js
@@ -1,14 +1,34 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
+// const { jwt_secret } = require("../config");
+// // Middleware for handling auth
+// function adminMiddleware(req, res, next) {
+//     // Implement admin auth logic
+//     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+//     const token = req.headers.authorization;
+//     const words = token.split(" ");
+//     const jwtToken = words[1];
+//     const decoded = jwt.verify(jwtToken, jwt_secret);
+//     if (decoded.username) {
+//         next();
+//     } else {
+//         res.status(403).json({
+//             msg: "You are not authenticated",
+//         });
+//     }
+// }
+
+const { jwt } = require("zod/v4");
 const { jwt_secret } = require("../config");
+
+// module.exports = adminMiddleware;
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
     const token = req.headers.authorization;
-    const words = token.split(" ");
-    const jwtToken = words[1];
+    const jwtToken = token.split(" ")[1];
     const decoded = jwt.verify(jwtToken, jwt_secret);
-    if (decoded.username) {
+    if (decoded) {
         next();
     } else {
         res.status(403).json({
--- week4/assignment-2/routes/admin.js
+++ week4/assignment-2/routes/admin.js
@@ -1,21 +1,86 @@
+// const { Router } = require("express");
+// const adminMiddleware = require("../middleware/admin");
+// const router = Router();
+// const jwt = require("jsonwebtoken");
+// const { jwt_secret } = require("../config");
+// const { Course } = require("../db");
+// const { Admin } = require("../db");
+// // Admin Routes
+// router.post("/signup", async (req, res) => {
+//     // Implement admin signup logic
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     await Admin.create({
+//         username: username,
+//         password: password,
+//     });
+//     res.json({
+//         msg: "admin created successfully",
+//     });
+// });
+
+// router.post("/signin", async (req, res) => {
+//     // Implement admin signup logic
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     const user = await Admin.find({
+//         username,
+//         password,
+//     });
+//     if (user) {
+//         const token = jwt.sign({ username }, jwt_secret);
+//         res.json({
+//             token,
+//         });
+//     } else {
+//         res.status(411).json({
+//             msg: "incorrect credentials",
+//         });
+//     }
+// });
+
+// router.post("/courses", adminMiddleware, async (req, res) => {
+//     // Implement course creation logic
+//     const title = req.body.title;
+//     const description = req.body.description;
+//     const imageLink = req.body.imageLink;
+//     const price = req.body.price;
+//     const newCourse = await Course.create({
+//         title,
+//         description,
+//         imageLink,
+//         price,
+//     });
+//     res.json({
+//         msg: "course created successfully ",
+//         courseId: newCourse._id,
+//     });
+// });
+
+// router.get("/courses", adminMiddleware, async (req, res) => {
+//     // Implement fetching all courses logic
+//     const response = await Course.find({});
+//     res.json({
+//         courses: response,
+//     });
+// });
+
+// module.exports = router;
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
-const router = Router();
-const jwt = require("jsonwebtoken");
+const { Admin, Course } = require("../../assignment-1/db");
+const { jwt } = require("zod/v4");
 const { jwt_secret } = require("../config");
-const { User, Course } = require("../db");
-const { Admin } = require("../db");
+const router = Router();
+
 // Admin Routes
 router.post("/signup", async (req, res) => {
     // Implement admin signup logic
     const username = req.body.username;
     const password = req.body.password;
     await Admin.create({
-        username: username,
-        password: password,
-    });
-    res.json({
-        msg: "admin created successfully",
+        username,
+        password,
     });
 });
 
@@ -23,11 +88,8 @@ router.post("/signin", async (req, res) => {
     // Implement admin signup logic
     const username = req.body.username;
     const password = req.body.password;
-    const user = await User.find({
-        username,
-        password,
-    });
-    if (user) {
+    const admin = await Admin.find({ username, password });
+    if (admin) {
         const token = jwt.sign({ username }, jwt_secret);
         res.json({
             token,
@@ -45,15 +107,15 @@ router.post("/courses", adminMiddleware, async (req, res) => {
     const description = req.body.description;
     const imageLink = req.body.imageLink;
     const price = req.body.price;
-    const newCourse = await Course.create({
+    const course = await Course.create({
         title,
         description,
         imageLink,
         price,
     });
-    res.json({
-        msg: "course created successfully ",
-        courseId: newCourse._id,
+    req.json({
+        msg: "course created",
+        courseId: course._id,
     });
 });
 
