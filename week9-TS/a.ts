f262c0775e8da125b1b425abbefeccc6bef28870
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-07-12 03:05:20 +0530

    TS step-4 donr=e
    week9

--- week9-TS/a.ts
+++ week9-TS/a.ts
@@ -1,2 +1,23 @@
-const x: number = 10;
-console.log(x);
+function Sum(a: number, b: number): number {
+    return a + b;
+}
+const sum = Sum(10, 20);
+console.log(sum);
+
+function greet(firstName: string, age: number) {
+    console.log("Hello " + firstName + " your age is " + age);
+}
+greet("Harshit", 20);
+
+function isLegal(age: number) {
+    return age > 18 ? true : false;
+}
+const legal = isLegal(18);
+console.log(legal);
+
+function dusra(pehla: () => void) {
+    setTimeout(pehla, 1000);
+}
+dusra(function () {
+    console.log("hello sir ");
+});
