a8e3ba0b1282c2bff4c1f31d40d52c911aa1289d
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-07-12 23:25:55 +0530

    step-6 interfaces

--- week9-TS/src/a.ts
+++ week9-TS/src/a.ts
@@ -9,11 +9,11 @@ function greet(firstName: string, age: number) {
 }
 greet("Harshit", 20);
 
-function isLegal(age: number) {
-    return age > 18 ? true : false;
-}
-const legal = isLegal(18);
-console.log(legal);
+// function isLegal(age: number) {
+//     return age > 18 ? true : false;
+// }
+// const legal = isLegal(18);
+// console.log(legal);
 
 function dusra(pehla: () => void) {
     setTimeout(pehla, 1000);
@@ -21,3 +21,49 @@ function dusra(pehla: () => void) {
 dusra(function () {
     console.log("hello sir ");
 });
+/*
+function isLegal(user: { firstName: string; lastName: string; age: number }) {
+    return user.age > 18 ? true : false;
+}
+isLegal({
+    firstName: "Harshit",
+    lastName: "Budhraja",
+    age: 20,
+});
+*/ // this is voilating the DRY rule
+// instead:
+
+interface User {
+    firstName: string;
+    lastName: string;
+    age: number;
+    email?: string;
+}
+function isLegal(user: User) {
+    return user.age > 18 ? true : false;
+}
+
+isLegal({
+    firstName: "Harshit",
+    lastName: "Budhraja",
+    age: 20,
+});
+
+interface Person {
+    name: string;
+    age: number;
+    greet(phrase: string): void;
+}
+class Employee implements Person {
+    name: string;
+    age: number;
+
+    constructor(n: string, a: number) {
+        this.name = n;
+        this.age = a;
+    }
+
+    greet(phrase: string) {
+        console.log(`${phrase} ${this.name}`);
+    }
+}
